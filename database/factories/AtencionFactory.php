dd4e43f9fb0ddd85d498d9c580391e629c52b2d7
devopsinf-pichilemu <136821917+devopsinf-pichilemu@users.noreply.github.com>
2024-02-27 10:45:24 -0300

    Atencion: Factory para crear atenciones de prueba.

--- database/factories/AtencionFactory.php
+++ database/factories/AtencionFactory.php
@@ -2,10 +2,14 @@
 
 namespace Database\Factories;
 
+use App\Models\Atencion;
+use App\Models\Sector;
+use App\Models\Tramite;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Parfaitementweb\FilamentCountryField\Forms\Components\Country;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Atencion>
+ * @extends Factory<Atencion>
  */
 class AtencionFactory extends Factory
 {
@@ -16,14 +20,21 @@ class AtencionFactory extends Factory
      */
     public function definition(): array
     {
-        return [
-            'rut' => fake()->text(50),
-            'names' => fake()->text(50),
-            'lastname_1' => fake()->text(50),
-            'lastname_2' => fake()->text(50),
-            'phone' => fake()->randomNumber(),
-            'nationality' => fake()->text(50),
+        $country = (fake()->numberBetween(0, 100) < 96) ? 'CL' : fake()->randomKey((new Country('list'))->getList());
+        $gender = fake()->randomElement(['female', 'male']);
+        $sector = Sector::all();
+        $tramite = Tramite::all();
 
+        return [
+            'rut' => fake()->rut(),
+            'names' => fake()->firstName($gender).' '.fake()->firstName($gender),  // 2 nombres.
+            'lastname_1' => fake()->lastName(),
+            'lastname_2' => fake()->lastName(),
+            'sector_id' => fake()->numberBetween($sector->first()->id, $sector->last()->id),
+            'phone' => '+56'.fake()->numberBetween(111111111, 999999999),
+            'nationality' => $country,
+            'tramite_id' => fake()->numberBetween($tramite->first()->id, $tramite->last()->id),
+            'created_at' => fake()->dateTimeBetween('-2 years'),
         ];
     }
 }
