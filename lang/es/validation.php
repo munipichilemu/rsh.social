7e3e3cf179635f38d242338d3969dbbc52e18063
devopsinf-pichilemu <136821917+devopsinf-pichilemu@users.noreply.github.com>
2024-03-04 18:13:29 -0300

    Localización: Textos de validación.

---
+++ lang/es/validation.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'custom' => [
+        'data.created_at' => [
+            'before_or_equal' => 'La fecha de atención no puede ser en el futuro.',
+        ],
+    ],
+];
---
+++ lang/vendor/rut/es/validation.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'rut' => 'El RUT ingresado no es válido.',
+    'strict' => 'Ingrese el RUT con puntos y guión.',
+    'exists' => 'El :attribute ya se encuentra registrado.',
+    'unique' => 'El :attribute ya se encuentra registrado.',
+];
