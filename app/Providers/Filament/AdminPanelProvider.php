65406a0625969b67277bb1a843027477eedc8710
devopsinf-pichilemu <136821917+devopsinf-pichilemu@users.noreply.github.com>
2024-03-04 17:34:51 -0300

    AdminPanel: Marca del RSH

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,6 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->brandName('Registro Social de Hogares')
             ->colors([
                 'primary' => Color::Indigo,
             ])
