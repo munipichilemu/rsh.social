c9a69be4be6cdcc0f784995434885fa74cb20593
devopsinf-pichilemu <136821917+devopsinf-pichilemu@users.noreply.github.com>
2024-03-11 14:29:08 -0300

    Fix: Importar Model en ShieldSeeder

--- database/seeders/ShieldSeeder.php
+++ database/seeders/ShieldSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 use BezhanSalleh\FilamentShield\Support\Utils;
 use Spatie\Permission\PermissionRegistrar;
